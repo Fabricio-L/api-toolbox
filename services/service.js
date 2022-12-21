ecc72caef89238c04b16a7516ee774f284673ad3
Fabricio Lukestik <fabri.lkt@gmail.com>
2022-12-21 03:30:49 -0300

    Add handler errors

--- services/service.js
+++ services/service.js
@@ -12,7 +12,16 @@ const options = {
 const listFiles = () => axios.request(options).then((response) => {
     return response.data;
   }).catch((error) => {
-    console.error(error);
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
   });
 
 const filesData = (file) => 
@@ -23,7 +32,16 @@ const filesData = (file) =>
   }).then((response) => {
     return response.data;
   }).catch((error) => {
-    console.error(error);
+    if (error.response) {
+      console.log(error.response.data);
+      console.log(error.response.status);
+      console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log('Error', error.message);
+    }
+    console.log(error.config);
   });
 
 module.exports = { listFiles, filesData }
